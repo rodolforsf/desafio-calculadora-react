877d1397e10584817f989c32cbb18bc2cd51721b
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-03-04 14:31:59 -0300

    As quatro operações básicas foram inseridas na calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: block;
+        direction: ltr;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+        color: #FFFFFF;
+    }
+`
